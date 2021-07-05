2838cc3c445e9fd0065c8bed7fd3e1c5730daaf8
jagadeesh <nakkajagadeesh1234@gmail.com>
2021-07-05 11:35:19 +0530

    Added code

--- src/App.js
+++ src/App.js
@@ -1,23 +1,124 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import data from "./users.json";
+import Form from "./form.js";
 
 function App() {
+  const [datas, setDatas] = React.useState([]);
+  const [userData] = React.useState(data.usersDetails);
+  const [searchData, setSearchData] = React.useState();
+  const [userDetails, setUserDetails] = React.useState({});
+  const [searchUser, setSearchUser] = React.useState("");
+  const [flag, setFlag] = React.useState(false);
+  const [formData, setFormData] = React.useState({
+    name: "",
+    emailID: "",
+    mobileNo: "",
+    gender: "",
+    address1: "",
+    address2: "",
+    city: "",
+    state: "",
+    pincode: "",
+    country: "",
+  });
+
+  const getUserDetails = (userName) => {
+    let newData = JSON.parse(
+      JSON.stringify(datas.length > 0 ? datas : userData)
+    );
+    setUserDetails(newData.filter((data) => data.name === userName)[0]);
+  };
+
+  const addData = () => {
+    setFlag(true);
+  };
+
+  const addUserData = () => {
+    setFlag(false);
+    if (datas.length > 0) {
+      setDatas([...datas, formData]);
+    } else {
+      setDatas([...userData, formData]);
+    }
+  };
+
+  const deleteData = () => {
+    let newData = JSON.parse(
+      JSON.stringify(datas.length > 0 ? datas : userData)
+    );
+    let updatedData = newData.filter((data) => data.name !== userDetails.name);
+    setDatas(updatedData);
+    setUserDetails({});
+  };
+
+  React.useEffect(() => {
+    let newData = JSON.parse(
+      JSON.stringify(datas.length > 0 ? datas : userData)
+    );
+    setSearchData(newData.filter((data) => data.name.includes(searchUser)));
+  }, [searchUser]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="users-Section">
+        {Object.keys(userDetails).length > 0 && (
+          <div>
+            <div className="delete-container" onClick={() => deleteData()}>
+              Delete
+            </div>
+          </div>
+        )}
+        <div className="inputBox">
+          <input
+            value={searchUser}
+            onChange={(e) => setSearchUser(e.target.value)}
+            placeholder="Search By Name"
+          />
+        </div>
+        <div className="usrList">
+          {(searchUser ? searchData : datas.length > 0 ? datas : userData).map(
+            (user) => (
+              <button
+                className="usrName"
+                onClick={() => getUserDetails(user.name)}
+              >
+                {user.name}
+              </button>
+            )
+          )}
+        </div>
+        <div>
+          <div className="add-container" onClick={() => addData()}>
+            Add User
+          </div>
+        </div>
+        {flag && (
+          <div className="user-form-container">
+            <div className="user-form-block">
+              <div>
+                <h2>Enter Details</h2>
+              </div>
+              <Form formData={formData} setFormData={setFormData} />
+              <div className="form-button">
+                <button onClick={() => addUserData()}>Submit</button>
+              </div>
+            </div>
+          </div>
+        )}
+      </div>
+      <div className="userDetail-Section">
+        {userDetails && (
+          <div className="details">
+            {Object.keys(userDetails).map((key) => (
+              <div>
+                <div style={{ fontWeight: "bold" }}>{key.toUpperCase()}</div>
+                <div>{userDetails[key]}</div>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
---
+++ src/form.js
@@ -0,0 +1,108 @@
+import React from "react";
+import "./form.css";
+
+function Form({ formData, setFormData }) {
+  const {
+    name,
+    emailID,
+    mobileNo,
+    gender,
+    address1,
+    address2,
+    city,
+    state,
+    pincode,
+    country,
+  } = formData;
+
+  const updateData = (e) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <form>
+      <div className="form-container">
+        <div>
+          <input
+            placeholder="Enter Name"
+            name="name"
+            value={name}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Mail Id"
+            name="emailID"
+            value={emailID}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Mobile No"
+            name="mobileNo"
+            value={mobileNo}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <select name="gender" id="gender" onChange={(e) => updateData(e)}>
+            <option value="male">Male</option>
+            <option value="female">Female</option>
+          </select>
+        </div>
+        <div>
+          <input
+            placeholder="Enter Address1"
+            name="address1"
+            value={address1}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Address2"
+            name="address2"
+            value={address2}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter City"
+            name="city"
+            value={city}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter State"
+            name="state"
+            value={state}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Pincode"
+            name="pincode"
+            value={pincode}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+        <div>
+          <input
+            placeholder="Enter Country"
+            name="country"
+            value={country}
+            onChange={(e) => updateData(e)}
+          />
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default Form;
